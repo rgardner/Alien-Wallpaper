530f0ab5659440a0e33bde7c946d6109d8a0a286
Robert Gardner <bob.hn.gardner@gmail.com>
2016-12-04 13:40:34 -0800

    Refactor into Post Class
    This separates all of the json handling into Post.from_json and
    fetch_posts. The non-parallel version of download_all_images has been
    removed because it was dead code. A Url phantom type is added to ensure
    it's always clear whether a subreddit is a name or a url.
    
    Two unit tests are added for the Post class with an valid post JSON and
    a self-post JSON.

--- alien_wallpaper.py
+++ alien_wallpaper.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 """
 Download images from Reddit.
 
@@ -9,8 +9,6 @@ $ alien_wallpaper --help
 """
 
 import argparse
-import codecs
-from functools import partial
 import json
 import logging
 import multiprocessing
@@ -18,115 +16,114 @@ import os.path
 import re
 import shutil
 import sys
+import typing
 import urllib.parse
 import urllib.request
 
 IMAGES_PER_SUBREDDIT = 25
 DEFAULT_SUBREDDITS = ['ArchitecturePorn', 'CityPorn', 'EarthPorn', 'SkyPorn',
                       'spaceporn', 'winterporn', 'quoteporn']
-
-FILE_EXTENSION_PROG = re.compile('.*.(jpg|jpeg|gif|png)$', re.IGNORECASE)
 USER_AGENT = 'AlienWallpaper by /u/WolfBlackout'
 
-logger = logging.getLogger(__name__)
+FILE_EXTENSION_PROG = re.compile('.*.(jpg|jpeg|gif|png)$', re.IGNORECASE)
+Url = typing.NewType('Url', str)
 
 
-def valid_post(post):
-    data = post['data']
-    try:
-        return not data['is_self'] and bool(FILE_EXTENSION_PROG.match(data['url']))
-    except KeyError:
-        logger.exception('valid_post: {}'.format(data))
-        return False
+class Post:
+    def __init__(self, post_id: str, is_self: bool, url: Url) -> None:
+        self.id = post_id
+        self.is_self = is_self
+        self.url = url
+
+    @property
+    def is_valid(self) -> bool:
+        return not self.is_self and bool(FILE_EXTENSION_PROG.match(self.url))
+
+    @property
+    def filename(self) -> typing.Optional[str]:
+        if self.extension is not None:
+            return self.id + '.' + self.extension
+
+    @property
+    def extension(self) -> typing.Optional[str]:
+        match = FILE_EXTENSION_PROG.match(self.url)
+        if match is not None:
+            return match.group(1)
+
+    def download(self, out_dir):
+        path = os.path.join(out_dir, self.filename)
+        logging.debug(path)
+
+        try:
+            with urllib.request.urlopen(self.url) as response, open(path, 'wb+') as out_file:
+                shutil.copyfileobj(response, out_file)
+        except urllib.error.HTTPError as e:
+            logging.exception(e)
+
+    @staticmethod
+    def from_json(data) -> 'Post':
+        return Post(data['id'], bool(data['is_self']), Url(data['url']))
 
 
-def filename(post):
-    reddit_id = post['data']['id']
-    ext = FILE_EXTENSION_PROG.match(post['data']['url']).group(1)
-    return reddit_id + '.' + ext
+def subreddit_to_url(subreddit: str) -> Url:
+    return Url('https://www.reddit.com/r/{}'.format(subreddit))
 
 
-def download_image(post, out_dir):
-    path = os.path.join(out_dir, filename(post))
-    logger.info(path)
-
-    url = post['data']['url']
-    try:
-        with urllib.request.urlopen(url) as response, open(path, 'wb+') as out_file:
-            shutil.copyfileobj(response, out_file)
-    except urllib.error.HTTPError as e:
-        logger.exception('download_image: {}'.format(e))
+def fetch_posts(subreddit: Url, limit=25, after=''):
+    params = urllib.parse.urlencode({'limit': limit, 'after': after})
+    url = '{}.json?{}'.format(subreddit, params)
+    req = urllib.request.Request(url, headers={'User-Agent': USER_AGENT})
+    resp = urllib.request.urlopen(req)
+    result = json.loads(resp.read().decode('utf-8'))
+    return [Post.from_json(p['data']) for p in result['data']['children']]
 
 
-def fetch_metadata(url, count=25, after=''):
-    params = urllib.parse.urlencode({'count': count, 'after': after})
-    url = url + '.json?' + params
-    req = urllib.request.Request(url=url, headers={'User-Agent': USER_AGENT})
-    reader = codecs.getreader('utf-8')
-    return json.load(reader(urllib.request.urlopen(req)))
-
-
-def download_images(subreddit, n, out_dir):
-    data = fetch_metadata(subreddit)
-    nimages = 0
+def download_images(subreddit_name, n, out_dir) -> None:
+    posts = fetch_posts(subreddit_name, limit=n)
+    num_images = 0
     while True:
-        for post in data['data']['children']:
-            if valid_post(post):
-                download_image(post, out_dir)
-                nimages += 1
-                if nimages >= n:
+        for post in posts:
+            if post.is_valid:
+                post.download(out_dir)
+                num_images += 1
+                if num_images >= n:
                     return
-        data = fetch_metadata(subreddit, after=data['data']['after'])
+
+        posts = fetch_posts(subreddit_name, limit=n, after=posts[-1].id)
 
 
-def subreddit_to_url(subreddit):
-    return 'https://www.reddit.com/r/{}'.format(subreddit)
-
-
-def download_all_images(subreddits, n, out_dir, par=True):
-    """
-    Download n images for each subreddit to out_dir.
-
-    :param par: use multiprocessing.Pool to download images in parallel
-
-    """
-    if par:
-        pool = multiprocessing.Pool()
-        download_images_to_dir = partial(download_images, n=n, out_dir=out_dir)
-        pool.map(download_images_to_dir, subreddits)
-        pool.close()
-        pool.join()
-    else:
+def download_all_images(subreddits: typing.Sequence[Url], n, out_dir):
+    """Download n images in parallel for each subreddit to out_dir."""
+    with multiprocessing.Pool() as pool:
         for subreddit in subreddits:
-            download_images(subreddit, n, out_dir)
+            pool.apply(download_images, args=(subreddit, n, out_dir))
 
 
 def parse_cli():
-    parser = argparse.ArgumentParser(description='Download Reddit images.')
+    parser = argparse.ArgumentParser(description='Download images from Reddit.')
     parser.add_argument('--subreddits', nargs='*')
-    parser.add_argument('--multireddit', nargs='?')
+    parser.add_argument('--multireddit', nargs='?', help='USER/multi_name')
     parser.add_argument('--out', required=True)
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true')
     return parser.parse_args()
 
 
 def main():
-    # initialize logger
-    logger.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-    # Parse CLI arguments to get subreddits and output directory
     args = parse_cli()
+    logging_level = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(stream=sys.stdout, level=logging_level)
 
-    # Make list of subreddit urls
     if not args.subreddits and not args.multireddit:
         subreddits = [subreddit_to_url(s) for s in DEFAULT_SUBREDDITS]
     else:
         subreddits = []
         if args.subreddits:
             subreddits.extend([subreddit_to_url(s) for s in args.subreddits])
+
         if args.multireddit:
             user, multi = args.multireddit.split('/')
-            url = 'https://www.reddit.com/user/{}/m/{}'.format(user, multi)
-            subreddits.append(url)
+            multi_url = 'https://www.reddit.com/user/{}/m/{}'.format(user, multi)
+            subreddits.append(multi_url)
 
     download_all_images(subreddits, IMAGES_PER_SUBREDDIT, args.out)
 
---
+++ tests.py
@@ -0,0 +1,222 @@
+import json
+import unittest
+import typing
+
+import alien_wallpaper
+from alien_wallpaper import Post, Url
+
+
+class TestPost:
+    def __init__(self, post: Post, filename: typing.Optional[str], json_data):
+        self.post = post
+        self.filename = filename
+        self.extension = filename.split('.')[1] if filename is not None else None
+        self.json = json.loads(json_data)
+
+
+test_post = TestPost(
+    Post('5gfjre', False, Url('http://i.imgur.com/XQgSj3o.jpg')),
+    '5gfjre.jpg',
+    """
+{
+    "contest_mode": false,
+    "banned_by": null,
+    "domain": "i.imgur.com",
+    "subreddit": "EarthPorn",
+    "selftext_html": null,
+    "selftext": "",
+    "likes": null,
+    "suggested_sort": null,
+    "user_reports": [
+
+    ],
+    "secure_media": null,
+    "saved": false,
+    "id": "5gfjre",
+    "gilded": 0,
+    "secure_media_embed": {
+
+    },
+    "clicked": false,
+    "report_reasons": null,
+    "author": "lukaas2",
+    "media": null,
+    "name": "t3_5gfjre",
+    "score": 3959,
+    "approved_by": null,
+    "over_18": false,
+    "removal_reason": null,
+    "hidden": false,
+    "preview": {
+        "images": [
+            {
+                "source": {
+                    "url": "https://i.redditmedia.com/QRosL3xOUwuzgDhm3SsmyBjYHrVH3w9B1lz101krEYg.jpg?s=d2ee7033a5824fa60f68b7c08c82cf81",
+                    "width": 5135,
+                    "height": 3327
+                },
+                "resolutions": [
+                    {
+                        "url": "https://i.redditmedia.com/QRosL3xOUwuzgDhm3SsmyBjYHrVH3w9B1lz101krEYg.jpg?fit=crop&amp;crop=faces%2Centropy&amp;arh=2&amp;w=108&amp;s=5d93c132b0c4654d234b1a0716a53235",
+                        "width": 108,
+                        "height": 69
+                    },
+                    {
+                        "url": "https://i.redditmedia.com/QRosL3xOUwuzgDhm3SsmyBjYHrVH3w9B1lz101krEYg.jpg?fit=crop&amp;crop=faces%2Centropy&amp;arh=2&amp;w=216&amp;s=de00d76fbebb5ba2307d0ae3e7fd5467",
+                        "width": 216,
+                        "height": 139
+                    },
+                    {
+                        "url": "https://i.redditmedia.com/QRosL3xOUwuzgDhm3SsmyBjYHrVH3w9B1lz101krEYg.jpg?fit=crop&amp;crop=faces%2Centropy&amp;arh=2&amp;w=320&amp;s=fb87cbe947e07a720598fd6e30c53d5a",
+                        "width": 320,
+                        "height": 207
+                    },
+                    {
+                        "url": "https://i.redditmedia.com/QRosL3xOUwuzgDhm3SsmyBjYHrVH3w9B1lz101krEYg.jpg?fit=crop&amp;crop=faces%2Centropy&amp;arh=2&amp;w=640&amp;s=8ee71f9f20d159f29f36ad0b2e4af4a5",
+                        "width": 640,
+                        "height": 414
+                    },
+                    {
+                        "url": "https://i.redditmedia.com/QRosL3xOUwuzgDhm3SsmyBjYHrVH3w9B1lz101krEYg.jpg?fit=crop&amp;crop=faces%2Centropy&amp;arh=2&amp;w=960&amp;s=65946023127783328b5498dce111f412",
+                        "width": 960,
+                        "height": 621
+                    },
+                    {
+                        "url": "https://i.redditmedia.com/QRosL3xOUwuzgDhm3SsmyBjYHrVH3w9B1lz101krEYg.jpg?fit=crop&amp;crop=faces%2Centropy&amp;arh=2&amp;w=1080&amp;s=5f1538ac08d1bee1ccb8c878c32a38a6",
+                        "width": 1080,
+                        "height": 699
+                    }
+                ],
+                "variants": {
+
+                },
+                "id": "2grwxIgMiygG4BM3VipcujVWm6iC7nn4PZndaY5oISw"
+            }
+        ]
+    },
+    "thumbnail": "http://a.thumbs.redditmedia.com/OsggFhJKhNraNrMFK-GvIKKFST9-1GH6uEBW1YdlNN4.jpg",
+    "subreddit_id": "t5_2sbq3",
+    "edited": false,
+    "link_flair_css_class": null,
+    "author_flair_css_class": "Camera",
+    "downs": 0,
+    "mod_reports": [
+
+    ],
+    "archived": false,
+    "media_embed": {
+
+    },
+    "post_hint": "image",
+    "is_self": false,
+    "hide_score": false,
+    "spoiler": false,
+    "permalink": "/r/EarthPorn/comments/5gfjre/verdon_gorge_france_provence_oc_5135x3327/",
+    "locked": false,
+    "stickied": false,
+    "created": 1480889161.0,
+    "url": "http://i.imgur.com/XQgSj3o.jpg",
+    "author_flair_text": null,
+    "quarantine": false,
+    "title": "Verdon Gorge, France (Provence) [OC, 5135x3327]",
+    "created_utc": 1480860361.0,
+    "link_flair_text": null,
+    "distinguished": null,
+    "num_comments": 52,
+    "visited": false,
+    "num_reports": null,
+    "ups": 3959
+}
+""")
+
+test_self_post = TestPost(
+    Post('5f9zc4', True, Url(
+        'https://www.reddit.com/r/rust/comments/5f9zc4/whats_everyone_working_on_this_week_422016/')),
+    None,
+    """
+{
+    "contest_mode": false,
+    "banned_by": null,
+    "domain": "self.rust",
+    "subreddit": "rust",
+    "selftext_html": "things",
+    "selftext": "Answer here or on the [rust-users thread](https://users.rust-lang.org/t/whats-everyone-working-on-this-week-42-2016/8188?u=llogiq)!",
+    "likes": null,
+    "suggested_sort": null,
+    "user_reports": [
+
+    ],
+    "secure_media": null,
+    "saved": false,
+    "id": "5f9zc4",
+    "gilded": 0,
+    "secure_media_embed": {
+
+    },
+    "clicked": false,
+    "report_reasons": null,
+    "author": "llogiq",
+    "media": null,
+    "name": "t3_5f9zc4",
+    "score": 20,
+    "approved_by": null,
+    "over_18": false,
+    "removal_reason": null,
+    "hidden": false,
+    "thumbnail": "",
+    "subreddit_id": "t5_2s7lj",
+    "edited": false,
+    "link_flair_css_class": null,
+    "author_flair_css_class": "contrib",
+    "downs": 0,
+    "mod_reports": [
+
+    ],
+    "archived": false,
+    "media_embed": {
+
+    },
+    "is_self": true,
+    "hide_score": false,
+    "spoiler": false,
+    "permalink": "/r/rust/comments/5f9zc4/whats_everyone_working_on_this_week_422016/",
+    "locked": false,
+    "stickied": true,
+    "created": 1480339845.0,
+    "url": "https://www.reddit.com/r/rust/comments/5f9zc4/whats_everyone_working_on_this_week_422016/",
+    "author_flair_text": "clippy · twir · rust · metacollect · flamer · overflower",
+    "quarantine": false,
+    "title": "What's everyone working on this week (42/2016)?",
+    "created_utc": 1480311045.0,
+    "link_flair_text": null,
+    "distinguished": "moderator",
+    "num_comments": 42,
+    "visited": false,
+    "num_reports": null,
+    "ups": 20
+}
+""")
+
+
+class TestAlienWallpaper(unittest.TestCase):
+    def test_post_from_json(self):
+        post = alien_wallpaper.Post.from_json(test_post.json)
+        self.assertEqual(post.id, test_post.post.id)
+        self.assertEqual(post.is_self, test_post.post.is_self)
+        self.assertEqual(post.url, test_post.post.url)
+        self.assertTrue(post.is_valid)
+        self.assertEqual(post.extension, test_post.extension)
+        self.assertEqual(post.filename, test_post.filename)
+
+    def test_self_post_from_json(self):
+        post = alien_wallpaper.Post.from_json(test_self_post.json)
+        self.assertEqual(post.id, test_self_post.post.id)
+        self.assertEqual(post.is_self, test_self_post.post.is_self)
+        self.assertEqual(post.url, test_self_post.post.url)
+        self.assertFalse(post.is_valid)
+        self.assertIsNone(post.extension)
+        self.assertIsNone(post.filename)
+
+
+if __name__ == '__main__':
+    unittest.main()
