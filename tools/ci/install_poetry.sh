0728d794c8bd31d334c2771473b900367ca3543b
Bob Gardner <bob.hn.gardner@gmail.com>
2020-01-20 15:02:23 -0500

    Add Poetry caching

--- tools/ci/install_poetry.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Installs poetry on Unix-like environment.
-
-set -euo pipefail
-
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
-source "${HOME}/.poetry/env"
